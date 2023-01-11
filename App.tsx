3c438a5611be1a00e24603f75f589ea7e18caae9
yuri-duque <yurithielmann83@gmail.com>
2023-01-10 22:55:16 -0300

    creating a footer and scrollview

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import { NativeBaseProvider } from 'native-base';
+import { Box, NativeBaseProvider, ScrollView, View } from 'native-base';
+import { Footer } from './src/components/Footer';
 import { Signup } from './src/screens/Sigup';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Signup />
+      <View style={{ maxHeight: 'calc(100vh - 50px)' }}>
+        <ScrollView>
+          <Signup />
+          <Signup />
+          <View h={5}></View>
+        </ScrollView>
+      </View>
+      <View minHeight={'50px'}>
+        <Footer />
+      </View>
     </NativeBaseProvider>
   );
 }
