3557f2a9d68edceb958cf221e7df0c96a0314bf7
yuri-duque <yurithielmann83@gmail.com>
2023-01-12 23:07:01 -0300

    creating a tab navigation with navigation router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { StackRoutes } from './stack.routes';
+import { TabRoutes } from './tabs.routes';
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <StackRoutes />
+      {/* <StackRoutes /> */}
+      <TabRoutes />
     </NavigationContainer>
   );
 }
