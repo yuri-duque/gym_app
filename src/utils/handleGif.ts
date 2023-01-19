df80c5831c2b3bc3fe33e11c3bf1d1e725af2748
yuri-duque <yurithielmann83@gmail.com>
2023-01-18 23:53:21 -0300

    inserting gif esteira

--- src/utils/handleGif.ts
+++ src/utils/handleGif.ts
@@ -6,6 +6,9 @@ export default function handleGif(gifName: string) {
     case 'supino-reto-aparelho':
       return require('../../assets/gifs/supino-reto-aparelho.gif');
 
+    case 'esteira':
+      return require('../../assets/gifs/esteira.gif');
+
     default:
       break;
   }
